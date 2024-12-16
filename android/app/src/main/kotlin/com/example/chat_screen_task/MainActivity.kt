aee1bc9553f61b083a637c3fa17258553acb494c
rohithchowdary929 <rohithchowdary929@gmail.com>
2024-12-16 15:29:54 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/chat_screen_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chat_screen_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:chat_screen_task/views/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: ChatScreen(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
